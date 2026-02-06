aba5b5a89c3b0d238ecaa9d6de755482967168a0
lost-seoul <62531661+lost-seoul@users.noreply.github.com>
2026-02-06 17:23:23 -0500

    Create config.h

---
+++ keyboards/swagkeys/eave/keymap/vial/config.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x5E, 0x43, 0xF8, 0xC3, 0x0F, 0x79, 0x34, 0x7A}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 15 }
+
+#define BOOTMAGIC_ROW 0
+#define BOOTMAGIC_COLUMN 0
+
+// Reduce firmware size by decreasing number of functions (use #undef to disable):
+#define DYNAMIC_KEYMAP_LAYER_COUNT 7
+#define DYNAMIC_KEYMAP_MACRO_COUNT 16
+#define VIAL_COMBO_ENTRIES 16
+#define VIAL_KEY_OVERRIDE_ENTRIES 16
+#define VIAL_TAP_DANCE_ENTRIES 16
+
+// Reduce firmware size by removing unneeded animations (use #undef to disable):
+#define RGBLIGHT_EFFECT_ALTERNATING
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_CHRISTMAS
+#define RGBLIGHT_EFFECT_KNIGHT
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_RGB_TEST
+#define RGBLIGHT_EFFECT_SNAKE
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#define RGBLIGHT_EFFECT_TWINKLE
+
+// deprecated RGBLIGHT commands
+#define RGB_MATRIX_DISABLE_SHARED_KEYCODES
